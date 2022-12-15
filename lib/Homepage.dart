b0ff19ff1bb4ea6119462d0133ce36b34d716306
nalsomali <naadaalsomali@gmail.com>
2022-12-15 22:00:23 +0300

    admin log in

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,3 +1,4 @@
+import 'package:cool_alert/cool_alert.dart';
 import 'package:web/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
@@ -16,22 +17,42 @@ class _HomeScreen extends State<HomeScreen> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        backgroundColor: Colors.white,
         appBar: AppBar(
+            backgroundColor: Colors.white,
             leading: Row(
-          children: [
-            IconButton(
-                icon: Icon(
-                  Icons.logout,
-                  color: Color.fromARGB(255, 45, 66, 142),
-                ),
-                onPressed: () async {
-                  await FirebaseAuth.instance.signOut();
-
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => UserLogin()));
-                })
-          ],
-        )));
+              children: [
+                IconButton(
+                    icon: const Icon(
+                      Icons.logout,
+                      color: Color.fromARGB(255, 45, 66, 142),
+                    ),
+                    onPressed: () {
+                      CoolAlert.show(
+                        context: context,
+                        title: "تسجيل الخروج",
+                        width: size.width * 0.2,
+                        confirmBtnColor: Color.fromARGB(181, 172, 22, 12),
+                        showCancelBtn: false,
+                        //cancelBtnColor: Color.fromARGB(144, 64, 6, 87),
+                        type: CoolAlertType.confirm,
+                        backgroundColor: Color.fromARGB(255, 45, 66, 142),
+                        text: "هل تريد تسجيل الخروج",
+                        confirmBtnText: 'تسجيل الخروج',
+                        cancelBtnText: "إلغاء",
+                        onCancelBtnTap: () {
+                          Navigator.pop(context);
+                        },
+                        onConfirmBtnTap: () async {
+                          await FirebaseAuth.instance.signOut();
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => UserLogin()));
+                        },
+                      );
+                    })
+              ],
+            )));
   }
 }
--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'backgroundLogin.dart';
 import 'Homepage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:form_field_validator/form_field_validator.dart';
@@ -35,179 +33,212 @@ class _UserLogin extends State<UserLogin> {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 255, 255, 255),
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-        ),
-        body: Background(
-          child: Form(
-            key: _FormKey,
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: size.height * 0.2),
-                Container(
-                  width: size.width * 0.25,
-                  margin: EdgeInsets.only(left: size.width * 0.75),
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: Text(
-                    "تسجيل الدخـول",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 45, 66, 142),
-                        fontSize: 34),
-                    textAlign: TextAlign.left,
-                  ),
+        automaticallyImplyLeading: false,
+      ),
+      body: Form(
+        key: _FormKey,
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: size.height * 0.2),
+            Container(
+              width: size.width * 0.25,
+              margin: EdgeInsets.only(left: size.width * 0.75),
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: const Text(
+                "تسجيل الدخـول",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 45, 66, 142),
+                    fontSize: 34),
+                textAlign: TextAlign.left,
+              ),
+            ),
+            SizedBox(height: size.height * 0.02),
+            Container(
+              width: size.width * 0.25,
+              margin: EdgeInsets.only(left: size.width * 0.7),
+              child: Positioned(
+                child: Image.asset(
+                  'images/logo.png',
+                  height: size.height * 0.2,
+                  width: size.width * 0.2,
                 ),
-                SizedBox(height: size.height * 0.02),
-                Container(
-                  width: size.width * 0.25,
-                  margin: EdgeInsets.only(left: size.width * 0.7),
-                  child: Positioned(
-                    child: new Image.asset(
-                      'images/logo.png',
-                      height: size.height * 0.2,
-                      width: size.width * 0.2,
-                    ),
+              ),
+            ),
+            Container(
+              width: size.width * 0.25,
+              margin: EdgeInsets.only(left: size.width * 0.7),
+              child: TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                onChanged: (value) {
+                  email = value;
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                    labelText: "البريد الإلكتروني",
+                    hintText: "example@email.com",
+                    hintStyle:
+                        TextStyle(color: Color.fromARGB(255, 202, 198, 198)),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    labelStyle: TextStyle(
+                      fontSize: 19,
+                      color: Color.fromARGB(255, 45, 66, 142),
+                    )),
+                validator:
+                    MultiValidator([RequiredValidator(errorText: 'إلزامي')]),
+              ),
+            ),
+            SizedBox(height: size.height * 0.03),
+            Container(
+              width: size.width * 0.25,
+              margin: EdgeInsets.only(left: size.width * 0.7),
+              child: TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "إلزامي";
+                  }
+                },
+                onChanged: (value) {
+                  password = value;
+                },
+                obscureText: showpass,
+                decoration: InputDecoration(
+                  labelText: "كلمة المرور",
+                  hintText: "********",
+                  hintStyle: const TextStyle(
+                      color: Color.fromARGB(255, 202, 198, 198)),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  labelStyle: TextStyle(
+                    fontSize: 19,
+                    color: Color.fromARGB(255, 45, 66, 142),
                   ),
-                ),
-                Container(
-                  width: size.width * 0.25,
-                  margin: EdgeInsets.only(left: size.width * 0.7),
-                  child: TextFormField(
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    onChanged: (value) {
-                      email = value;
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        showpass = !showpass;
+                      });
                     },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        labelText: "البريد الإلكتروني",
-                        hintText: "example@email.com",
-                        hintStyle: TextStyle(
-                            color: Color.fromARGB(255, 202, 198, 198)),
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                        labelStyle: TextStyle(
-                          fontSize: 19,
-                          color: Color.fromARGB(255, 45, 66, 142),
-                        )),
-                    validator: MultiValidator(
-                        [RequiredValidator(errorText: 'إلزامي')]),
+                    child: showpass
+                        ? Icon(
+                            Icons.visibility_off,
+                            color: Color.fromARGB(255, 45, 66, 142),
+                          )
+                        : Icon(
+                            Icons.visibility,
+                            color: Color.fromARGB(255, 45, 66, 142),
+                          ),
                   ),
                 ),
-                SizedBox(height: size.height * 0.03),
-                Container(
-                  width: size.width * 0.25,
-                  margin: EdgeInsets.only(left: size.width * 0.7),
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "إلزامي";
-                      }
-                    },
-                    onChanged: (value) {
-                      password = value;
-                    },
-                    obscureText: showpass,
-                    decoration: InputDecoration(
-                      labelText: "كلمة المرور",
-                      hintText: "********",
-                      hintStyle:
-                          TextStyle(color: Color.fromARGB(255, 202, 198, 198)),
-                      floatingLabelBehavior: FloatingLabelBehavior.always,
-                      labelStyle: TextStyle(
-                        fontSize: 19,
-                        color: Color.fromARGB(255, 45, 66, 142),
-                      ),
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            showpass = !showpass;
-                          });
-                        },
-                        child: showpass
-                            ? const Icon(
-                                Icons.visibility_off,
-                              )
-                            : Icon(
-                                Icons.visibility,
-                              ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: size.height * 0.03),
-                Container(
-                  child: Container(
-                    width: size.width * 0.2,
-                    margin: EdgeInsets.only(left: size.width * 0.7),
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        if (_FormKey.currentState!.validate()) {
-                          try {
-                            final user = await _auth.signInWithEmailAndPassword(
-                                email: email, password: password);
-                            if (user != null) {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => HomeScreen()));
+              ),
+            ),
+            SizedBox(height: size.height * 0.03),
+            Container(
+              width: size.width * 0.2,
+              margin: EdgeInsets.only(left: size.width * 0.7),
+              child: ElevatedButton(
+                onPressed: () async {
+                  if (_FormKey.currentState!.validate()) {
+                    try {
+                      final user = await _auth.signInWithEmailAndPassword(
+                          email: email, password: password);
+                      if (user != null) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeScreen()));
 
-                              print("تم تسجيل الدخول بنجاح");
-                            }
-                          } catch (e) {
-                            print(e);
-                            /*   validator:
-                      MultiValidator([
-                        RequiredValidator(
-                            errorText: 'Incorrect username or password')
-                      ]);*/
-                            CoolAlert.show(
-                              context: context,
-                              title: "نعتذر",
-                              width: size.width * 0.2,
-                              confirmBtnColor: Color.fromARGB(255, 45, 66, 142),
-                              //cancelBtnColor: Color.fromARGB(144, 64, 6, 87),
-                              type: CoolAlertType.error,
-                              backgroundColor: Color.fromARGB(255, 45, 66, 142),
-                              text: "البريد الالكتروني او كلمة المرور خاطئة",
-                              confirmBtnText: 'حاول مره اخرى',
-                              onConfirmBtnTap: () {
-                                Navigator.of(context).pop();
-                              },
-                            );
-                          }
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)),
-                        backgroundColor: Colors.white,
-                        padding: const EdgeInsets.all(0),
-                      ),
-                      child: Container(
-                        alignment: Alignment.center,
-                        height: 50.0,
-                        width: size.width * 0.5,
-                        decoration: new BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            gradient: new LinearGradient(colors: [
-                              Color.fromARGB(255, 45, 66, 142),
-                              Color.fromARGB(255, 45, 66, 142),
-                            ])),
-                        padding: const EdgeInsets.all(0),
-                        child: Text(
-                          "تسجيـل الـدخـول",
-                          style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromARGB(255, 255, 255, 255)),
-                        ),
-                      ),
-                    ),
+                        print("تم تسجيل الدخول بنجاح");
+                      }
+                    } catch (e) {
+                      print(e);
+                      /*   validator:
+                    MultiValidator([
+                      RequiredValidator(
+                          errorText: 'Incorrect username or password')
+                    ]);*/
+                      CoolAlert.show(
+                        context: context,
+                        title: "نعتذر",
+                        width: size.width * 0.2,
+                        confirmBtnColor: Color.fromARGB(255, 45, 66, 142),
+                        //cancelBtnColor: Color.fromARGB(144, 64, 6, 87),
+                        type: CoolAlertType.error,
+                        backgroundColor: Color.fromARGB(255, 45, 66, 142),
+                        text: "البريد الالكتروني او كلمة المرور خاطئة",
+                        confirmBtnText: 'حاول مره اخرى',
+                        onConfirmBtnTap: () {
+                          Navigator.of(context).pop();
+                        },
+                      );
+                    }
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.all(0),
+                ),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 50.0,
+                  width: size.width * 0.5,
+                  decoration: new BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      gradient: new LinearGradient(colors: [
+                        Color.fromARGB(255, 45, 66, 142),
+                        Color.fromARGB(255, 45, 66, 142),
+                      ])),
+                  padding: const EdgeInsets.all(0),
+                  child: Text(
+                    "تسجيـل الـدخـول",
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 255, 255, 255)),
                   ),
                 ),
-              ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  final Widget child;
+
+  const Background({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Positioned(
+            height: size.height * 0.5,
+            width: size.width * 0.5,
+            right: size.width * 0.5,
+            child: new Image.asset(
+              'images/Admin2.png',
+              height: size.height * 0.5,
+              width: size.width * 0.5,
             ),
           ),
-        ));
+        ],
+      ),
+    );
   }
 }
