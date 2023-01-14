0c69139a08d2fd5d6a9cef5e66dcf93992952f4d
Layan Alwadie <layanalwadie22@gmail.com>
2023-01-14 21:13:12 +0300

    add map page

--- lib/addMap.dart
+++ lib/addMap.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:cool_alert/cool_alert.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:web/login.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:web/loading_manager.dart';
 
 class addMap extends StatefulWidget {
   const addMap({Key? key}) : super(key: key);
@@ -13,6 +19,9 @@ class addMap extends StatefulWidget {
 class _addMapState extends State<addMap> {
   //setting the expansion function for the navigation rail
   bool isExpanded = false;
+  File? _pickedImage;
+  Uint8List webImage = Uint8List(8);
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -70,9 +79,90 @@ class _addMapState extends State<addMap> {
                 ),
               ],
               selectedIndex: 0),
+
+          Container(
+              height: 500,
+              width: 400,
+              decoration: BoxDecoration(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: _pickedImage == null
+                  ? dottedBorder(color: Colors.black)
+                  : ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: kIsWeb
+                          ? Image.memory(webImage, fit: BoxFit.fill)
+                          : Image.file(_pickedImage!, fit: BoxFit.fill),
+                    )),
         ],
       ),
       //let's add the floating action button
+    );
+  }
+
+  Future<void> _pickImage() async {
+    if (!kIsWeb) {
+      final ImagePicker _picker = ImagePicker();
+      XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        var selected = File(image.path);
+        setState(() {
+          _pickedImage = selected;
+        });
+      } else {
+        print('لم تختر صورة');
+      }
+    } else if (kIsWeb) {
+      final ImagePicker _picker = ImagePicker();
+      XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        var f = await image.readAsBytes();
+        setState(() {
+          webImage = f;
+          _pickedImage = File('a');
+        });
+      } else {
+        print('لم تختر صورة');
+      }
+    } else {
+      print('حدث خطأ');
+    }
+  }
+
+  Widget dottedBorder({
+    required Color color,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: DottedBorder(
+          dashPattern: const [6.7],
+          borderType: BorderType.RRect,
+          color: color,
+          radius: const Radius.circular(12),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.image_outlined,
+                  color: color,
+                  size: 50,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextButton(
+                    onPressed: (() {
+                      _pickImage();
+                    }),
+                    child: Text(
+                      "اضغط هنا لإضافة صورة الخريطة",
+                    ))
+              ],
+            ),
+          )),
     );
   }
 }
